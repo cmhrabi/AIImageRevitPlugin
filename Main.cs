35eb70100a66faaf9d50cc9d63c90a881e002754
Calum Hrabi <chrabi@uwaterloo.ca>
2023-06-25 11:02:05 -0400

    first commit

---
+++ Main.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.ApplicationServices;
+using System.IO;
+
+namespace AIImageRevitPlugin
+{
+    public class Main
+    {
+        [Transaction(TransactionMode.Manual)]
+        public class WallFilter : IExternalCommand
+        {
+            public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+            {
+                UIApplication uiApp = commandData.Application;
+                UIDocument uiDoc = uiApp.ActiveUIDocument;
+                Application app = uiApp.Application;
+                Document doc = uiDoc.Document;
+
+                var categories = new List<BuiltInCategory>()
+            {
+                BuiltInCategory.OST_Floors,
+                BuiltInCategory.OST_Walls,
+            };
+
+                var multiCategoryFilter = new ElementMulticategoryFilter(categories);
+                var collector = new FilteredElementCollector(doc)
+                     .WherePasses(multiCategoryFilter);
+
+                return Result.Succeeded;
+            }
+        }
+
+        [Transaction(TransactionMode.Manual)]
+        public class MainFunction : IExternalCommand
+        {
+            public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+            {
+                UIApplication uiApp = commandData.Application;
+                UIDocument uiDoc = uiApp.ActiveUIDocument;
+                Application app = uiApp.Application;
+                Document doc = uiDoc.Document;
+
+                var element = uiDoc.Selection.GetElementIds().Select(
+                    x => doc.GetElement(x)).First();
+
+                var value = element.LookupParameter("Comments").AsString();
+                TaskDialog.Show("Message", value);
+
+                return Result.Succeeded;
+            }
+        }
+
+        [Transaction(TransactionMode.Manual)]
+        public class Export_JPG : IExternalCommand
+        {
+            public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+            {
+                UIApplication uiApp = commandData.Application;
+                UIDocument uiDoc = uiApp.ActiveUIDocument;
+                Application app = uiApp.Application;
+                Document doc = uiDoc.Document;
+
+                string fileName = "exported_image.jpg";
+
+                var BilledeExportOptions = new ImageExportOptions
+                {
+                    ZoomType = ZoomFitType.FitToPage,
+                    PixelSize = 2024,
+                    FitDirection = FitDirectionType.Horizontal,
+                    FilePath = Environment.CurrentDirectory + @"\Data\" + fileName,
+                    HLRandWFViewsFileType = ImageFileType.JPEGLossless,
+                    ImageResolution = ImageResolution.DPI_600,
+                };
+
+                doc.ExportImage(BilledeExportOptions);
+
+                return Result.Succeeded;
+            }
+        }
+    }
+}
+}
---
+++ StableDiffusionAPIConnector.cs
@@ -0,0 +1,43 @@
+﻿using RestSharp.Authenticators;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp.Authenticators.OAuth2;
+
+namespace AIImageRevitPlugin
+{
+    internal class StableDiffusionAPIConnector
+    {
+        public string prompt = "default prompt";
+        public string name;
+        public string filePath;
+        public RestClient client;
+
+        public StableDiffusionAPIConnector(string prompt, string name)
+        {
+            var fileName = "exported_image.jpg";
+            this.prompt = prompt;
+            this.name = name;
+            this.filePath = Environment.CurrentDirectory + @"\Data\" + fileName;
+
+            var authenticatior = new OAuth2AuthorizationRequestHeaderAuthenticator(token, "Bearer");
+            var options = new RestClientOptions("https://api.twitter.com/1.1")
+            {
+                Authenticator = authenticatior,
+            };
+            client = new RestClient(options);
+
+
+        }
+
+        public void fetchImageToImage()
+        {
+
+            Console.WriteLine("Succesfully Configured API Connection");
+        }
+    }
+}
+}
